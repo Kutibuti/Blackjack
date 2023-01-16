2f5701718739481e379b9e08c704eebde9ca1ed6
Ömer Faruk Ayaz <omerfayaz166@gmail.com>
2023-01-16 17:55:04 +0300

    Basic improvments about getting a new card.

--- index.js
+++ index.js
@@ -1,38 +1,50 @@
 let firstCard = 10
-let secondCard = 11
-let sum = firstCard + secondCard
-let age = 19
+let secondCard = 4
+
+let cards = [firstCard, secondCard]
+let sum = cards[0] + cards[1]
 let hasBlackjack = false
 let isAlive = true
 let message = ""
+let messageEl = document.getElementById("message-el")
+let cardsEl = document.getElementById("cards-el")
+//let sumEl= document.getElementById("sum-el")
+let sumEl = document.querySelector("#sum-el")
 
 
+function renderGame() {
+    cardsEl.textContent = "Cards:"
 
-function blackJack() {
+    for(let i=0; i<cards.length; i++) {
+        cardsEl.textContent += " " + cards[i]
+    }
+    sumEl.textContent = "Sum: " + sum
     if (sum <= 20) {
         message = "Do you want to draw a new card?"
+        messageEl.textContent = message
     } else if (sum === 21) {
-        message = "You've BJ"
+        message = "You've got BJ"
+        messageEl.textContent = message
         hasBlackjack = true
     } else {
         message = "You're out of the game."
+        messageEl.textContent = message
         isAlive = false
     }
 }
 
 
-function ageCheck() {
-    if (age >= 18) {
-        console.log("Welcome you can play blackjack")
-        blackJack()
-    } else {
-        console.log("Sorry you can't enter the casino!")
-    }
-}
-
 function startGame() {
-    ageCheck()
+    renderGame()
     console.log(hasBlackjack)
     console.log(message)
+}
+
+
+function newCard() {
+    let newcard = 7
+    cards.push(newcard)
+    sum += newcard
+    renderGame()
 }
 
