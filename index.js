0a31cca16960c870584b0c7f820da6e45b139c33
Ömer Faruk Ayaz <omerfayaz166@gmail.com>
2023-01-18 00:26:20 +0300

    blabla

--- index.js
+++ index.js
@@ -1,21 +1,15 @@
-let firstCard = 10
-let secondCard = 4
-
-let cards = [firstCard, secondCard]
-let sum = cards[0] + cards[1]
+let cards = []
+let sum = 0
 let hasBlackjack = false
-let isAlive = true
 let message = ""
 let messageEl = document.getElementById("message-el")
 let cardsEl = document.getElementById("cards-el")
-//let sumEl= document.getElementById("sum-el")
+//let sumEl = document.getElementById("sum-el")
 let sumEl = document.querySelector("#sum-el")
-
 
 function renderGame() {
     cardsEl.textContent = "Cards:"
-
-    for(let i=0; i<cards.length; i++) {
+    for (let i = 0; i < cards.length; i++) {
         cardsEl.textContent += " " + cards[i]
     }
     sumEl.textContent = "Sum: " + sum
@@ -27,6 +21,7 @@ function renderGame() {
         messageEl.textContent = message
         hasBlackjack = true
     } else {
+
         message = "You're out of the game."
         messageEl.textContent = message
         isAlive = false
@@ -35,16 +30,57 @@ function renderGame() {
 
 
 function startGame() {
+    let isAlive = true
+    for (let i = 0; i < 2; i++) {
+        cards[i] = getRandomCard()
+    }
+    sum = cards[0] + cards[1]
     renderGame()
     console.log(hasBlackjack)
     console.log(message)
+
 }
 
 
 function newCard() {
-    let newcard = 7
+    let newcard = getRandomCard()
     cards.push(newcard)
     sum += newcard
-    renderGame()
+    if (isAlive === true) {
+        renderGame()
+    }
+    if (sum > 21) {
+        isAlive = false
+
+    }
 }
+
+function getRandomCard() {
+    let randomcard = Math.floor(Math.random() * 13) + 1
+    if (randomcard > 10) {
+        randomcard = 10
+    } else if (randomcard === 1) {
+        randomcard = 11
+    }
+    return randomcard
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
