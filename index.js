fe253002452df1cc85703d85f501ba2051b512a1
Ömer Faruk Ayaz <omerfayaz166@gmail.com>
2023-01-20 01:35:40 +0300

    Adding dealer and some conditions

--- index.js
+++ index.js
@@ -1,64 +1,76 @@
-let player = {
+/*let player = {
     name: "Omer",
     chips: 150
 }
-
-player.firstmethod()
+*/
 
 let cards = []
-let sum = 0
-let hasBlackjack = false
+let dealerCard = []
+let yourSum = 0
+let dealerSum = 0
+let hasBlackJack = false
 let isAlive = true
+let drawAgain = true
 let message = ""
-let messageEl = document.getElementById("message-el")
-let cardsEl = document.getElementById("cards-el")
-//let sumEl = document.getElementById("sum-el")
-let sumEl = document.querySelector("#sum-el")
-let playerEl = document.getElementById("player-el")
+const messageEl = document.getElementById("message-el")
+const cardsEl = document.getElementById("cards-el")
+const dealerEl = document.querySelector("#dealer-el")
+const playerEl = document.getElementById("player-el")
+const stayEl = document.getElementById("btn-stay")
+const newCardEl = document.getElementById("btn-addcard")
+const startEl = document.getElementById("btn-start")
+
+stayEl.addEventListener("click", function () {
+    calculate()
+})
+
+newCardEl.addEventListener("click", function () {
+    if (isAlive === true && hasBlackJack === false) {
+        let newcard = getRandomCard()
+        cards.push(newcard)
+        yourSum += newcard
+        renderGame()
+    }
+})
+
+startEl.addEventListener("click", function () {
+    if (isAlive === true) {
+        for (let i = 0; i < 2; i++) {
+            cards[i] = getRandomCard()
+        }
+        dealerCard[0] = getRandomCard()
+        yourSum += cards[0] + cards[1]
+        dealerSum += dealerCard[0]
+        renderGame()
+    }
+})
 
 
-playerEl.textContent = player.name + ": $" + player.chips
+//playerEl.textContent = player.name + ": $" + player.chips
 
 function renderGame() {
-    cardsEl.textContent = "Cards:"
+    cardsEl.textContent = "Your Hand:"
+    dealerEl.textContent = "Dealer's Hand: " + dealerCard[0]
+
     for (let i = 0; i < cards.length; i++) {
         cardsEl.textContent += " " + cards[i]
     }
-    sumEl.textContent = "Sum: " + sum
-    if (sum <= 20) {
+    if (yourSum <= 20) {
         message = "Do you want to draw a new card?"
         messageEl.textContent = message
-    } else if (sum === 21) {
+    } else if (yourSum === 21) {
         message = "You've got BJ"
         messageEl.textContent = message
-        hasBlackjack = true
+        hasBlackJack = true
     } else {
-        message = "You're out of the game."
+        message = "You've lost!"
         messageEl.textContent = message
         isAlive = false
     }
 }
 
 
-function startGame() {
-    if (isAlive === true) {
-        for (let i = 0; i < 2; i++) {
-            cards[i] = getRandomCard()
-        }
-        sum = cards[0] + cards[1]
-        renderGame()
-    }
-}
 
-
-function newCard() {
-    if (isAlive === true && hasBlackJack === false) {
-        let newcard = getRandomCard()
-        cards.push(newcard)
-        sum += newcard
-        renderGame()
-    }
-}
 
 function getRandomCard() {
     let randomcard = Math.floor(Math.random() * 13) + 1
@@ -70,6 +82,39 @@ function getRandomCard() {
     return randomcard
 }
 
+function calculate() {
+    if (drawAgain === true) {
+        for (let i = 1; i<10; i++) {
+            dealerCard[i] = getRandomCard()
+            dealerEl.textContent += " " + dealerCard[i]
+            dealerSum += dealerCard[i]
+            if (dealerSum > 16 && dealerSum < 22) {
+                break
+            }
+        }
+        drawAgain = false
+    }
+
+    if (dealerSum === 21) {
+        if (dealerSum === yourSum) {
+            message = "Draw!"
+            messageEl.textContent = message
+        }
+        else {
+            message = "You've lost!"
+            messageEl.textContent = message
+        }
+    } else if (dealerSum > 16 && dealerSum < 21) {
+        if (dealerSum < yourSum && yourSum <= 21) {
+            message = "You've win!"
+            messageEl.textContent = message
+        }
+        else {
+            message = "You've lost!"
+            messageEl.textContent = message
+        }
+    }
+}
 
 
 
